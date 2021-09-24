11255f6ef87219a8877fa6e9030035dc97cba0af
Geetaprakash <86463646+Geetaprakash@users.noreply.github.com>
2021-09-24 09:34:50 +0530

    Print Z

---
+++ Zalpa.java
@@ -0,0 +1,17 @@
+public class Zalpa
+{
+public static void main(String arga[])
+{
+int i,j;
+for(i=1;i<=5;i++){
+for(j=1;j<5;j++)
+{
+if(i==1 || i==5 || i+j == 6)
+System.out.print(" *");
+else
+System.out.print(" ");
+}
+System.out.println();
+}
+}
+}
